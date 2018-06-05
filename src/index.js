1179a349683169e07287883e0b7a72f9c20aa5d9
raj594 <rajaynes@gmail.com>
2018-06-04 21:26:21 -0500

    fix nav bar width, install redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker();
